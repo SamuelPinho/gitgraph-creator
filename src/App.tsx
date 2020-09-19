fe67ae1f442ac4123eb72d0e143297f57260a78e
Samuel Monteiro <samuel.pinho@fatec.sp.gov.br>
2020-09-19 17:41:10 -0300

    add some styles and fix some problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Gitgraph } from "@gitgraph/react";
+import { Commit } from "@gitgraph/core";
 import {
   Flex,
   Button,
@@ -15,107 +16,457 @@ import {
   Input,
   ModalFooter,
   Select,
+  Divider,
+  Text,
+  Code,
+  Stack,
+  FormErrorMessage,
+  Box,
+  RadioButtonGroup,
+  ButtonProps,
 } from "@chakra-ui/core";
 import { UseDisclosureReturn } from "@chakra-ui/core/dist/useDisclosure";
-import { useGitContext, CreateBranchModalData } from "./context/git";
+import {
+  useGitContext,
+  CreateBranchModalData,
+  ReactSvgElement,
+  AddComitToBranchModalData,
+  MergeBranchModalData,
+} from "./context/git";
 import { useForm } from "react-hook-form";
 
 export default function App() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { graph } = useGitContext();
+  const { graph, selectedCommit } = useGitContext();
+  const [modalName, setModalName] = React.useState<
+    "createBranch" | "addCommit" | "mergeBranch"
+  >();
 
-  const initialRef = React.useRef<HTMLInputElement>();
+  const initialRef = React.useRef<HTMLElement>();
   const finalRef = React.useRef<HTMLInputElement>(null);
 
   return (
-    <Flex m="20px">
-      <Flex>
-        <Button onClick={onOpen} variantColor="green">
-          Criar branch
-        </Button>
+    <Flex width="100%" m="20px" p="10px">
+      <Flex
+        width="100%"
+        flexDirection="column"
+        height="100%"
+        maxWidth="200px"
+        mr="3"
+      >
+        <Flex>
+          <Button
+            onClick={() => {
+              setModalName("createBranch");
+              onOpen();
+            }}
+            variantColor="green"
+          >
+            Criar branch
+          </Button>
+        </Flex>
+        {selectedCommit && (
+          <>
+            <Divider />
+            <Flex>
+              <SelectedCommitOptions
+                commit={selectedCommit}
+                openAddCommitModal={() => {
+                  setModalName("addCommit");
+                  onOpen();
+                }}
+                openMergeBranchModal={() => {
+                  setModalName("mergeBranch");
+                  onOpen();
+                }}
+              />
+            </Flex>
+          </>
+        )}
       </Flex>
-      <Gitgraph graph={graph} />
+      <Box shadow="sm" borderRadius="md" p={2} bg="gray.200">
+        <Gitgraph graph={graph} />
+      </Box>
       {isOpen && (
         <Modal
           initialFocusRef={initialRef as React.RefObject<HTMLInputElement>}
           finalFocusRef={finalRef}
           isOpen={isOpen}
           onClose={onClose}
+          closeOnOverlayClick={false}
         >
-          <CreateBranchModal onClose={onClose} initialRef={initialRef} />
+          <ModalOverlay />
+
+          {modalName === "createBranch" && (
+            <CreateBranchModal onClose={onClose} initialRef={initialRef} />
+          )}
+          {modalName === "addCommit" && (
+            <AddCommitToBranchModal onClose={onClose} initialRef={initialRef} />
+          )}
+          {modalName === "mergeBranch" && (
+            <MergeBranchModal onClose={onClose} initialRef={initialRef} />
+          )}
         </Modal>
       )}
     </Flex>
   );
 }
 
+const SelectedCommitOptions = ({
+  commit,
+  openAddCommitModal,
+  openMergeBranchModal,
+}: {
+  commit: Commit<ReactSvgElement>;
+  openAddCommitModal: () => void;
+  openMergeBranchModal: () => void;
+}) => {
+  const branch = commit.branches || [];
+
+  const branchName = branch[0];
+
+  return (
+    <Stack flexDirection="column" spacing={3}>
+      <Flex flexDirection="column">
+        <Text color="gray.600" fontWeight="600">
+          Selected Branch
+        </Text>
+        <Code variantColor="green" children={branchName} width="fit-content" />
+      </Flex>
+      <Flex flexDirection="column">
+        <Text color="gray.600" fontWeight="600">
+          Actions
+        </Text>
+        <Stack>
+          <Button onClick={openAddCommitModal} size="sm" variantColor="gray">
+            commit
+          </Button>
+          <Button onClick={openMergeBranchModal} size="sm" variantColor="gray">
+            merge
+          </Button>
+        </Stack>
+      </Flex>
+    </Stack>
+  );
+};
+
+const CustomRadio = React.forwardRef<
+  ButtonProps,
+  ButtonProps & {
+    isChecked?: boolean;
+    value?: string;
+    children: React.ReactNode;
+  }
+>((props, ref) => {
+  const { isChecked, isDisabled, value, children, ...rest } = props;
+  return (
+    <>
+      <Button
+        variantColor={isChecked ? "blue" : "gray"}
+        size="sm"
+        aria-checked={isChecked}
+        role="radio"
+        isDisabled={isDisabled}
+        ref={ref}
+        mb={2}
+        {...rest}
+      >
+        {children}
+      </Button>
+    </>
+  );
+});
+
 function CreateBranchModal({
   onClose,
   initialRef,
 }: {
-  initialRef: React.MutableRefObject<HTMLInputElement | undefined>;
+  initialRef: React.MutableRefObject<HTMLElement | undefined>;
 } & Pick<UseDisclosureReturn, "onClose">) {
-  const { graph, createBranch } = useGitContext();
-  const { handleSubmit, register, reset } = useForm<CreateBranchModalData>();
+  const { graph, createBranch, selectedCommit } = useGitContext();
+
+  const branchesArray = Array.from(graph.branches.keys());
+  // const defaultBaseBranch = branchesArray[branchesArray.length - 1];
+  const getDefaultBaseBranch = React.useCallback(() => {
+    const defaultBranch = branchesArray[branchesArray.length - 1];
+
+    if (!selectedCommit) return defaultBranch;
+
+    const branch = selectedCommit.branches || defaultBranch;
+
+    return branch[0];
+  }, [branchesArray, selectedCommit]);
+
+  const { handleSubmit, register, reset, errors, setError, setValue } = useForm<
+    CreateBranchModalData
+  >({
+    defaultValues: {
+      baseBranch: getDefaultBaseBranch(),
+    },
+  });
 
   const onSubmit = handleSubmit(
-    async ({ branchName, baseBranch, firstCommitMessage }) => {
-      createBranch({ branchName, baseBranch, firstCommitMessage });
+    ({ baseBranch, branchName, firstCommitMessage }) => {
+      console.log(baseBranch);
+
+      if (!baseBranch) {
+        setError("baseBranch", {
+          message: "baseBranch cannot be null",
+        });
+      }
+
+      if (branchName === baseBranch) {
+        setError("branchName", {
+          message: "branch name cannot be equal to base branch",
+        });
+        setError("baseBranch", {
+          message: "branch name cannot be equal to base branch",
+        });
+        return;
+      }
+
+      createBranch({ baseBranch, branchName, firstCommitMessage });
 
       reset();
     }
   );
 
+  React.useEffect(() => {
+    if (!setValue) return;
+    setValue("baseBranch", getDefaultBaseBranch());
+  }, [getDefaultBaseBranch, setValue]);
+
   return (
     <>
-      <ModalOverlay />
       <ModalContent>
         <form onSubmit={onSubmit}>
           <ModalHeader>Create a branch</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
+            <Stack spacing={2}>
+              <FormControl isInvalid={!!errors.branchName}>
+                <FormLabel htmlFor="branchName">Branch Name</FormLabel>
+                <Input
+                  name="branchName"
+                  id="branchName"
+                  ref={(e: HTMLInputElement) => {
+                    register({
+                      required: {
+                        value: true,
+                        message: "required",
+                      },
+                    })(e);
+                    initialRef.current = e;
+                  }}
+                />
+                <FormErrorMessage>
+                  {errors.branchName?.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="baseBranch">Create from branch</FormLabel>
+                <Input
+                  ref={register}
+                  name="baseBranch"
+                  id="baseBranch"
+                  display="none"
+                />
+                <RadioButtonGroup
+                  defaultValue={getDefaultBaseBranch()}
+                  onChange={(val) => setValue("baseBranch", val as any)}
+                  isInline
+                  spacing={2}
+                  key={branchesArray.length}
+                >
+                  {branchesArray.map((branchName) => (
+                    <CustomRadio value={branchName} key={branchName}>
+                      {branchName}
+                    </CustomRadio>
+                  ))}
+                </RadioButtonGroup>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="firstCommitMessage">
+                  First Commit Message
+                </FormLabel>
+                <Input
+                  name="firstCommitMessage"
+                  id="firstCommitMessage"
+                  defaultValue="feature/first commit message"
+                  ref={register}
+                />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variantColor="blue" mr={3} onClick={onSubmit} type="submit">
+              Create
+            </Button>
+            <Button onClick={onClose} type="button">
+              Cancel
+            </Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </>
+  );
+}
+
+function MergeBranchModal({
+  onClose,
+  initialRef,
+}: {
+  initialRef: React.MutableRefObject<HTMLElement | undefined>;
+} & Pick<UseDisclosureReturn, "onClose">) {
+  const { graph, mergeBranch, selectedCommit } = useGitContext();
+
+  const branchesArray = Array.from(graph.branches.keys());
+
+  const { handleSubmit, register, reset, watch } = useForm<
+    MergeBranchModalData
+  >({
+    defaultValues: {
+      branchToMergeName: branchesArray[0],
+    },
+  });
+
+  const onSubmit = handleSubmit(async ({ branchToMergeName }) => {
+    mergeBranch({ branchToMergeName });
+
+    reset();
+  });
+
+  const originBranch = selectedCommit?.branches || "";
+  const hasSelectedBranch = watch().branchToMergeName !== undefined;
+
+  return (
+    <>
+      <ModalContent>
+        <form onSubmit={onSubmit}>
+          <ModalHeader
+            flexDirection="row"
+            alignItems="cente"
+            justifyContent="center"
+          >
+            Merge branch&nbsp;
+            <Code
+              variantColor="green"
+              children={originBranch[0]}
+              verticalAlign="middle"
+            />
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
             <FormControl>
-              <FormLabel htmlFor="branchName">Branch Name</FormLabel>
-              <Input
-                name="branchName"
-                id="branchName"
-                ref={(e: HTMLInputElement) => {
+              <FormLabel htmlFor="branchToMergeName">Destiny Branch</FormLabel>
+              <Select
+                name="branchToMergeName"
+                id="branchToMergeName"
+                placeholder="Choose a destiny branch"
+                ref={(e: HTMLSelectElement) => {
                   register({ required: true })(e);
                   initialRef.current = e;
                 }}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="baseBranch">Base Branch</FormLabel>
-              <Select
-                name="baseBranch"
-                id="baseBranch"
-                placeholder="Choose a base branch"
-                ref={register}
+                defaultValue="master"
               >
-                {Array.from(graph.branches.keys()).map((branchName, index) => (
+                {branchesArray.map((branchName) => (
                   <option key={branchName} value={branchName}>
                     {branchName}
                   </option>
                 ))}
               </Select>
             </FormControl>
+            <Stack
+              spacing={1}
+              isInline
+              mt="20px"
+              width="100%"
+              alignItems="center"
+            >
+              <Text fontWeight="600" color="gray.600">
+                Will merge
+              </Text>
+              <Code variantColor="green" children={originBranch[0]} />
+              <Text fontWeight="600" color="gray.600">
+                to
+              </Text>
+              {hasSelectedBranch && (
+                <Code
+                  variantColor="blue"
+                  children={watch().branchToMergeName}
+                />
+              )}
+            </Stack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variantColor="blue" mr={3} onClick={onSubmit} type="submit">
+              Merge
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </>
+  );
+}
+
+function AddCommitToBranchModal({
+  onClose,
+  initialRef,
+}: {
+  initialRef: React.MutableRefObject<HTMLElement | undefined>;
+} & Pick<UseDisclosureReturn, "onClose">) {
+  const { createCommit, selectedCommit } = useGitContext();
+  const { handleSubmit, register, reset } = useForm<
+    AddComitToBranchModalData
+  >();
+
+  const branchName = selectedCommit?.branches || "";
+
+  const onSubmit = handleSubmit(async ({ commitMessage }) => {
+    if (!commitMessage) return;
+
+    createCommit({ commitMessage });
+
+    reset();
+  });
+
+  return (
+    <>
+      <ModalContent>
+        <form onSubmit={onSubmit}>
+          <ModalHeader
+            flexDirection="row"
+            alignItems="cente"
+            justifyContent="center"
+          >
+            Add commit to branch&nbsp;
+            <Code
+              variantColor="green"
+              children={branchName[0]}
+              verticalAlign="middle"
+            />
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
             <FormControl>
-              <FormLabel htmlFor="firstCommitMessage">
-                First Commit Message
-              </FormLabel>
+              <FormLabel htmlFor="commitMessage">Commit Message</FormLabel>
               <Input
-                name="firstCommitMessage"
-                id="firstCommitMessage"
-                defaultValue="feature/first commit message"
-                ref={register}
+                name="commitMessage"
+                id="commitMessage"
+                ref={(e: HTMLInputElement) => {
+                  register({ required: true })(e);
+                  initialRef.current = e;
+                }}
               />
             </FormControl>
           </ModalBody>
 
           <ModalFooter>
             <Button variantColor="blue" mr={3} onClick={onSubmit} type="submit">
-              Create
+              Create commit
             </Button>
             <Button onClick={onClose}>Cancel</Button>
           </ModalFooter>
--- src/context/git.tsx
+++ src/context/git.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import {
   GitgraphCore,
   GitgraphUserApi,
@@ -8,7 +14,7 @@ import {
   Commit,
 } from "@gitgraph/core";
 
-type ReactSvgElement = React.ReactElement<SVGElement>;
+export type ReactSvgElement = React.ReactElement<SVGElement>;
 
 export type CreateBranchModalData = {
   branchName: string;
@@ -16,11 +22,22 @@ export type CreateBranchModalData = {
   firstCommitMessage: string;
 };
 
+export type AddComitToBranchModalData = {
+  commitMessage: string;
+};
+
+export type MergeBranchModalData = {
+  branchToMergeName: string;
+};
+
 type GitState = {
   graph: GitgraphCore<ReactSvgElement>;
   graphAPI: GitgraphUserApi<ReactSvgElement>;
+  selectedCommit: Commit<ReactSvgElement> | undefined;
 
   createBranch: (attrs: CreateBranchModalData) => void;
+  createCommit: (attrs: AddComitToBranchModalData) => void;
+  mergeBranch: (attrs: MergeBranchModalData) => void;
 };
 
 const GitContext = createContext<GitState | undefined>(undefined);
@@ -38,17 +55,75 @@ const graphTemplate = templateExtend(TemplateName.Metro, {
   },
 });
 
+const graph = new GitgraphCore<ReactSvgElement>({
+  branchLabelOnEveryCommit: true,
+  author: "Samuel",
+  orientation: Orientation.VerticalReverse,
+  template: graphTemplate,
+});
+
+const handleChangeDotFillColor = (element: HTMLElement, color: string) => {
+  element.style.fill = "white";
+  element.style.stroke = "#4299E1";
+  element.style.strokeWidth = "8px";
+};
+
+const resetElement = (element: HTMLElement, color: string) => {
+  element.style.fill = color;
+  element.style.strokeWidth = "0";
+};
+
+const getCommitElement = (
+  hash: Commit<ReactSvgElement>["hash"]
+): HTMLElement | null => {
+  const elementId = hash;
+  const element = document.getElementById(elementId);
+
+  if (!element) return null;
+
+  return element;
+};
+
 const GitProvider = ({ children }: GitProviderProps) => {
-  const graph = new GitgraphCore<ReactSvgElement>({
-    branchLabelOnEveryCommit: true,
-    author: "Samuel",
-    orientation: Orientation.VerticalReverse,
-    template: graphTemplate,
-  });
   const graphAPI = graph.getUserApi();
   const [selectedCommit, setSelectedCommit] = useState<
     Commit<ReactSvgElement> | undefined
   >();
+  const [prevColor, setPrevColor] = useState<string | undefined>();
+  const [_, setPrevHash] = useState<string | undefined>();
+
+  useEffect(() => {
+    graphAPI.branch("master").commit("feature/first commit message");
+  }, []);
+
+  useEffect(() => {
+    if (!selectedCommit || !document) return;
+
+    const element = getCommitElement(selectedCommit.hash);
+
+    if (!element) return;
+
+    const color = element.attributes.getNamedItem("fill")?.textContent;
+
+    if (!color) return;
+
+    setPrevHash((prev) => {
+      if (prev && prevColor && selectedCommit.hash !== prev) {
+        const prevElement = getCommitElement(prev);
+        if (prevElement) resetElement(prevElement, prevColor);
+      }
+
+      return selectedCommit.hash;
+    });
+
+    setPrevColor(color);
+
+    handleChangeDotFillColor(element, "red");
+  }, [prevColor, selectedCommit]);
+
+  const handleOnClick = (commit: Commit<ReactSvgElement>) => {
+    setSelectedCommit(commit);
+  };
 
   const createBranch = ({
     baseBranch,
@@ -58,20 +133,72 @@ const GitProvider = ({ children }: GitProviderProps) => {
     if (!branchName || !firstCommitMessage) return;
 
     if (baseBranch) {
-      graphAPI.branch(baseBranch).branch(branchName).commit({
+      const branch = graphAPI.branch(baseBranch).branch(branchName);
+
+      branch.commit({
         subject: firstCommitMessage,
-        onClick: setSelectedCommit,
+        onClick: handleOnClick,
       });
     } else {
-      graphAPI.branch(branchName).commit({
+      const branch = graphAPI.branch(branchName);
+
+      branch.commit({
         subject: firstCommitMessage,
-        onClick: setSelectedCommit,
+        onClick: handleOnClick,
+        onMouseOver: (c) => {
+          const element = getCommitElement(c.hash);
+          if (!element) return;
+
+          console.log(element);
+
+          element.style.boxShadow = "0 2px 6px 0 rgba(0,0,0,0.1)";
+        },
       });
     }
   };
 
+  const createCommit = ({ commitMessage }: AddComitToBranchModalData) => {
+    if (!selectedCommit || !commitMessage) return;
+    if (!selectedCommit.branches) return;
+
+    const branch = graph.branches.get(selectedCommit.branches[0]);
+
+    if (!branch) return;
+
+    branch.getUserApi().commit({
+      subject: commitMessage,
+      onClick: handleOnClick,
+    });
+  };
+
+  const mergeBranch = ({ branchToMergeName }: MergeBranchModalData) => {
+    if (!selectedCommit || !branchToMergeName) return;
+    if (!selectedCommit.branches) return;
+
+    const destinyBranch = graph.branches.get(branchToMergeName);
+    const originBranch = graph.branches.get(selectedCommit.branches[0]);
+
+    if (!destinyBranch || !originBranch) return;
+
+    destinyBranch.getUserApi().merge({
+      branch: originBranch.getUserApi().name,
+      commitOptions: {
+        onClick: handleOnClick,
+      },
+    });
+  };
+
   return (
-    <GitContext.Provider value={{ graph, graphAPI, createBranch }}>
+    <GitContext.Provider
+      value={{
+        graph,
+        graphAPI,
+        createBranch,
+        selectedCommit,
+        createCommit,
+        mergeBranch,
+      }}
+    >
       {children}
     </GitContext.Provider>
   );
